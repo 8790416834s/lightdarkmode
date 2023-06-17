e9c615bc1122f5cac3846ce83830347ec1fa700e
sravankumar <cheripellysravan0411@gmail.com>
2023-06-17 12:01:57 +0000

    error message

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -3,41 +3,39 @@ import {Component} from 'react'
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {mode: 'Light Mode'}
+  state = {mode: "LightMode"}
 
     onLight = () => {
-      this.setState(prevState => ({mode: 'Light Mode'}))
+      this.setState(prevState => ({prevState.mode: prevState."LightMode"}))
     }
 
     onDark = () => {
-      this.setState(prevState => ({mode: 'Dark Mode'}))
+      this.setState(prevState => ({prevState.mode: prevState."DarkMode"}))
     }
   }
 
-  render() {
+  render() {  
       const { mode } = this.state
-        let authButton;
-        if (mode === 'Light Mode') {
-            authButton =  (<div className="dark-container">
-          <h1 className="dark-heading">Click To Change Mode</h1>
-          <div>
-            <button type="button" className="dark-button" onClick={this.onDark}>
-              {mode}
-            </button>
-          </div>
-        </div>)
-        } else if (mode === 'Dark Mode') {
-            authButton = (<div className="light-container">
-        <h1 className="light-heading">Click To Change Mode</h1>
-        <div>
-          <button type="button" className="light-button" onClick={this.onLight}>
-            {mode}
-          </button>
-        </div>
-      </div>)
-        }
 
-    return <div className="container">{this.authButton}</div>
+    return (
+    <div className="container">
+    {mode === "LightMode" ? (<div className="dark-container">
+                <h1 className="dark-heading">Click To Change Mode</h1>
+                <div>
+                    <button type="button" className="dark-button" onClick={this.onDark}>
+                    {mode}
+                    </button>
+                </div>
+                </div>) : (<div className="light-container">
+                <h1 className="light-heading">Click To Change Mode</h1>
+                <div>
+                <button type="button" className="light-button" onClick={this.onLight}>
+                    {mode}
+                </button>
+                </div>
+            </div>)}
+    </div>
+    )
   }
 }
 
